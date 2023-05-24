a3120b0e95e360dfb3b53389ed4f064dd0d30141
tluxxx <tobias.luecke@gmx.net>
2023-05-24 18:24:17 +0200

    upload

---
+++ 01_modules_classes/math_help_02.py
@@ -0,0 +1,27 @@
+# some mathematical functions
+
+import math
+
+def exponential(x):
+  return math.exp(x)
+
+def sinus(x):
+  return math.sin(x*math.pi)
+
+def root(x):
+  return math.sqrt(x)
+
+def square(x):
+  return x**2
+
+def kehr(x):
+  return 1/x-1
+
+def tangens(x):
+  return math.tan(x*math.pi)
+
+def cubic(x):
+  return x**3
+
+def cosinus(x):
+  return math.cos(x*math.pi)
